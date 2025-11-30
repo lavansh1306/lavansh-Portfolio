56ac1c2789bdb9fef2c27f9e157f33eb35460425
lavansh1306 <lavansh1201a@gmail.com>
2025-11-30 14:32:08 +0530

    chore: allow opening DevTools by removing devtools-protect call

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react'
-import installDevtoolsProtections from './lib/devtools-protect'
 import { ParallaxProvider } from 'react-scroll-parallax'
 
 const root = createRoot(document.getElementById('root')!);
@@ -11,7 +10,6 @@ root.render(
   <ParallaxProvider>
     <App />
     {process.env.NODE_ENV === 'production' && typeof Analytics !== 'undefined' ? <Analytics /> : null}
-    {/* Install lightweight devtools protections (deterrent only) */}
-    {typeof window !== 'undefined' ? (installDevtoolsProtections(), null) : null}
+    {/* DevTools protections removed to allow opening browser DevTools with shortcuts */}
   </ParallaxProvider>
 );
